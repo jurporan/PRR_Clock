9b3c65923d0dc4826e4511b6d24fd944b0f12694
benoistwolleb <planetben1@gmail.com>
2015-10-28 21:45:01 +0100

    Commentaires du main

--- src/nouveau_plus_simple/Slave.java
+++ src/nouveau_plus_simple/Slave.java
@@ -1,8 +1,20 @@
+/*
+    Auteurs :       Jan Purro et Benois Wolleb
+    Projet :        PRR_Clock
+    Fichier :       Slave.java
+    Description :   Cette classe contient le programme principal du laboratoire. Elle lance simplement un client PTP et affiche périodiquement l'heure synchronisée.
+*/
+
 
 public class Slave
 {
+    /*
+        Paramètre : -args : Arguments de la ligne de commande (non utilisés)
+        Description : Crée un client PTP et affiche périodiquement l'heure synchronisée
+    */
     public static void main(String[] args)
     {
+        // Construction du client PTP
         PTPSlave slave = new PTPSlave();
         
         System.out.println("Heure avant le lancement de PTPSlave: " + slave.getTime());
@@ -13,7 +25,7 @@ public class Slave
             try {Thread.sleep(2000);}
             catch (Exception e) {}
             
-            //System.out.println("Heure: " + slave.getTime());
+            System.out.println("Heure: " + slave.getTime());
         }
     }
 }
