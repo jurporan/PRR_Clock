c7b35ef01654183f7259a6c77e84f91b42ba24c5
jurporan <jan.purro@heig-vd.ch>
2015-10-28 20:31:59 +0100

    Entêtes exemples dans PTPMaster

--- src/nouveau_plus_simple/PTPMaster.java
+++ src/nouveau_plus_simple/PTPMaster.java
@@ -1,14 +1,27 @@
+/*
+Auteurs : Jan Purro et Benois Wolleb
+Projet : PRR_Clock
+Fichier : PTPMaster.java
+Description : Classe qui fait un truc.
+*/
+
 import java.net.*;
 import java.io.*;
 import java.nio.*;
 import java.util.*;
+
 
 public class PTPMaster extends Thread
 {
     private MulticastSocket socket;
     private InetAddress group;
     private Heartbeat heartbeat;
-    
+
+    /*
+    Constructeur
+    Paramètre : -
+    Description : Construit l'objet...
+    */
     public PTPMaster()
     {
         try
@@ -21,42 +34,42 @@ public class PTPMaster extends Thread
         {
             System.out.println("Impossible d'accéder au réseau");
         }
-        
+
         heartbeat = new Heartbeat(socket, group);
     }
-    
+
     public void run()
     {
         heartbeat.start();
-        
+
         byte[] inputBuffer = new byte[(Byte.SIZE + Character.SIZE + Long.SIZE) / Byte.SIZE];
         DatagramPacket packet = new DatagramPacket(inputBuffer, inputBuffer.length);
-        
+
         byte[] data;
         ByteBuffer bf;
         byte type;
         char no;
         Long receivedNanotime;
-        
+
         byte[] outputBuffer = new byte[(Byte.SIZE + Character.SIZE + Long.SIZE) / Byte.SIZE];
         outputBuffer[0] = Protocol.DELAY_RESPONSE;
         DatagramPacket delayResponse = new DatagramPacket(outputBuffer, outputBuffer.length);
         delayResponse.setPort(Protocol.slavePort);
         byte[] receivedNanotimeArray;
-        
+
         while (true)
         {
             try {socket.receive(packet);}
             catch (Exception e) {}
-            
+
             receivedNanotime = System.nanoTime();
             data = packet.getData();
             bf = ByteBuffer.wrap(data);
             type = data[0];
             no = bf.getChar(1);
-            
+
             System.out.println("Paquet reçu, type " + new Integer(type));
-            
+
             switch(type)
             {
                 case Protocol.DELAY_REQUEST:
@@ -77,7 +90,7 @@ public class PTPMaster extends Thread
             }
         }
     }
-    
+
     public Long getTime()
     {
         return System.nanoTime();
