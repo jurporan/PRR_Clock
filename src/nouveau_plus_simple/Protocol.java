5a8327cc597363a8b2939faf3b7a74127d49b875
benoistwolleb <planetben1@gmail.com>
2015-10-29 10:36:58 +0100

    Commentaires du Protocol

--- src/nouveau_plus_simple/Protocol.java
+++ src/nouveau_plus_simple/Protocol.java
@@ -1,3 +1,10 @@
+/*
+    Auteurs :       Jan Purro et Benois Wolleb
+    Projet :        PRR_Clock
+    Fichier :       Protocol.java
+    Description :   Cette classe sert uniquement de conteneur pour les constantes du protocole PTP. Nous avons décidé de placer les constantes de type des messages dans un byte et non de faire un type énuméré, ce qui aurait été plus logique. Cependant, en Java, les types énumérés sont stockés sur 4 octets alors que nous avons besoin de très peu de valeurs (4 au total). Nous avons donc choisi d'utiliser un byte, soit un seul octet pour stocker ces valeurs, ce qui diminue la taille de la trame à transmettre.
+*/
+
 public class Protocol
 {
     public static final byte SYNC = 1;
